d30a39722c6080c356a40c7b3835fa4b282714a0
fahimmuntasir669 <muntasirmozumder689@gmail.com>
2025-08-07 12:53:19 +0600

    added

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 // Define a service using a base URL and expected endpoints
 export const baseApi = createApi({
     reducerPath: 'baseAPi',
-    baseQuery: fetchBaseQuery({ baseUrl: 'https://library-management-app-xi.vercel.app/api/' }),
+    baseQuery: fetchBaseQuery({ baseUrl: 'https://library-management-app-rust.vercel.app/api/' }),
     tagTypes: ["Books", "BorrowBooks"],
     endpoints: (build) => ({
         getBookByName: build.query({
